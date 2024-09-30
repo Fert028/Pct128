b698021ade8fbdb70975953048321e670d629be5
Fert028 <hack101white@gmail.com>
2024-09-30 20:22:46 +0300

    adaptive (width > 1600px)

--- src/pages/ContactsPage/Contacts.jsx
+++ src/pages/ContactsPage/Contacts.jsx
@@ -7,43 +7,46 @@ export default function Contacts() {
     <div className={style.container}>
       <h1 className="title">Контакты</h1>
 
-      <section className={style.PctConatactCard}>
-        <div className={style.pctLinks}>
-          <h2>ЗАО "Прогрессивные Химические Технологии"</h2>
-          <ul>
-            <li>
-              <a
-                href="mailto:zaopht1996@gmail.com"
-                title="Написать нам на почту"
-                className="link"
-              >
-                zaopht1996@gmail.com
-              </a>
-            </li>
-            <br />
-            <li>
-              <a
-                href="https://yandex.ru/maps/-/CHHgUxa"
-                title="Посмотреть на картах"
-                target="_blank"
-                className="link"
-              >
-                117545, г. Москва, <br /> 1-й Дорожный проезд, дом 7
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className={style.genereal}>
-          <a href="/" className={style.logoPct}>
-            <img src={logo} alt="" className="logoPct" />
-          </a>
-          <div>
-            <h4>Генеральный директор:</h4>
-            <h2>Вячеслав Иванович Зотов</h2>
-            <br />
-            <a href="mailto:progchemtech@gmail.com" className="link">
-              progchemtech@gmail.com
+      <section>
+        <div className={style.PctConatactCard}>
+
+          <div className={style.pctLinks}>
+            <h2>ЗАО "Прогрессивные Химические Технологии"</h2>
+            <ul>
+              <li>
+                <a
+                  href="mailto:zaopht1996@gmail.com"
+                  title="Написать нам на почту"
+                  className="link"
+                >
+                  zaopht1996@gmail.com
+                </a>
+              </li>
+              <br />
+              <li>
+                <a
+                  href="https://yandex.ru/maps/-/CHHgUxa"
+                  title="Посмотреть на картах"
+                  target="_blank"
+                  className="link"
+                >
+                  117545, г. Москва, <br /> 1-й Дорожный проезд, дом 7
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={style.genereal}>
+            <a href="/" className={style.logoPct}>
+              <img src={logo} alt="" className="logoPct" />
             </a>
+            <div>
+              <h4>Генеральный директор:</h4>
+              <h2>Вячеслав Иванович Зотов</h2>
+              <br />
+              <a href="mailto:progchemtech@gmail.com" className="link">
+                progchemtech@gmail.com
+              </a>
+            </div>
           </div>
         </div>
       </section>
